ee320516a880d500954679b94401cfe7a9ca7727
ivan-f78 <blue-eyesrasky@hotmail.com>
2011-09-22 12:09:32 -0500

    Practica 1

--- practicas/Pared.java
+++ practicas/Pared.java
@@ -14,10 +14,10 @@ public class Pared extends World {
     public Pared()
     {
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(1024, 600, 1);
         GreenfootImage background = getBackground();
-        background.setColor(Color.BLACK);
+        background.setColor(Color.BLUE);
         background.fill();
-        addObject(new Reloj(), 300, 200);
+        addObject(new Reloj(), 512, 300);
     }
 }
--- practicas/Reloj.java
+++ practicas/Reloj.java
@@ -9,10 +9,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Reloj extends Actor {
 
     private Manecilla _horas;
+    
+    private Manecilla _minutos;
 
     public void addedToWorld(World world) {
-        _horas = new Manecilla(12);
+        _horas = new Manecilla(10);
         world.addObject(_horas, getX(), getY());
+        
+        _minutos = new Manecilla(120);
+        world.addObject(_minutos, getX(), getY());
     }
 
     /**
@@ -20,5 +25,8 @@ public class Reloj extends Actor {
      */
     public void act()
     {
+        _horas .incrementa();
+        
+        _minutos .incrementa();
     }
 }
